80195126a9860ea49cbf0a8d3e0ff55a1325d51f
Andrew Murray <radarhere@users.noreply.github.com>
2019-08-17 19:34:43 +1000

    Simplifications

--- Tests/test_file_gif.py
+++ Tests/test_file_gif.py
@@ -472,7 +472,7 @@ class TestFileGif(PillowTestCase):
             except EOFError:
                 pass
 
-    def test_partially_identical_frames(self):
+    def test_identical_frames(self):
         duration_list = [1000, 1500, 2000, 4000]
 
         out = self.tempfile("temp.gif")
@@ -495,37 +495,23 @@ class TestFileGif(PillowTestCase):
         # Assert that the new duration is the total of the identical frames
         self.assertEqual(reread.info["duration"], 4500)
 
-    def test_totally_identical_frames(self):
+    def test_identical_frames_to_single_frame(self):
         duration_list = [1000, 1500, 2000, 4000]
 
         out = self.tempfile("temp.gif")
-
-        image_path = "Tests/images/bc7-argb-8bpp_MipMaps-1.png"
         im_list = [
-            Image.open(image_path),
-            Image.open(image_path),
-            Image.open(image_path),
-            Image.open(image_path),
+            Image.new("L", (100, 100), "#000"),
+            Image.new("L", (100, 100), "#000"),
+            Image.new("L", (100, 100), "#000"),
         ]
-        mask = Image.new("RGBA", im_list[0].size, (255, 255, 255, 0))
-
-        frames = []
-        for image in im_list:
-            frames.append(Image.alpha_composite(mask, image))
 
         # duration as list
-        frames[0].save(
-            out,
-            save_all=True,
-            append_images=frames[1:],
-            optimize=False,
-            duration=duration_list,
-            loop=0,
-            transparency=0,
+        im_list[0].save(
+            out, save_all=True, append_images=im_list[1:], duration=duration_list
         )
         reread = Image.open(out)
 
-        # Assert that all four frames were combined
+        # Assert that all frames were combined
         self.assertEqual(reread.n_frames, 1)
 
         # Assert that the new duration is the total of the identical frames
